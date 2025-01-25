a89abde447bc85a22ec6d5502ac296526b21130f
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2025-01-25 20:32:20 +0100

    chore: Updated the implementation for google analytics

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -169,22 +169,22 @@ const config: Config = {
 
 const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID;
 
-config.scripts = [
-  {
-    src: `https://www.googletagmanager.com/gtm.js?id=${GOOGLE_ANALYTICS_ID}`,
-    async: true,
-  },
-  {
-    innerHTML: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', '${GOOGLE_ANALYTICS_ID}');
-    `,
-    type: 'text/javascript',
-    async: true,
-  },
-];
-
+if (GOOGLE_ANALYTICS_ID) {
+  config.scripts = [
+    {
+      src: `https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`,
+      async: true,
+    },
+    {
+      innerHTML: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${GOOGLE_ANALYTICS_ID}');
+      `,
+      type: 'text/javascript',
+    },
+  ];
+}
 
 export default config;
