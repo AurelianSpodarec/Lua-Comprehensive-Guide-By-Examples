9619f1fc0bfcac247bd7d85d8d81914b23d6d087
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2025-01-25 01:08:57 +0100

    chore: Added blog back and my first blog

---
+++ blog/2025-01-25-welcome/index.md
@@ -0,0 +1,8 @@
+---
+slug: welcome
+title: Welcome
+authors: [aurelian]
+tags: [lua, documentation]
+---
+
+Hello!
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -146,10 +146,10 @@ const config: Config = {
         {
           title: 'More',
           items: [
-            // {
-            //   label: 'Blog',
-            //   to: '/blog',
-            // },
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
             {
               label: 'GitHub',
               href: 'https://github.com/AurelianSpodarec/LuaDocs',
